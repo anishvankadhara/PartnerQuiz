640ca53e2f2d291e40bea005ae92f44291f5ad79
akullladdha <akul.laddha@gmail.com>
2024-11-26 20:51:04 -0500

    added lambda expression
    implemented into Main.java

--- PartnerQuizJava/src/Main.java
+++ PartnerQuizJava/src/Main.java
@@ -2,14 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Interface addition = (a,b)->{
+            int count=0;
+            for(int i=a; i<=b; i++) {
+                count+=i;
+            }
+            return count;
+        };
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try {
+            int a  = Integer.parseInt(args[0]);
+            int b = Integer.parseInt(args[1]);
+            System.out.println(addition.add(a,b));
+        } catch (NumberFormatException e) {
+            System.out.println("error, enter integers");
         }
+
     }
 }
